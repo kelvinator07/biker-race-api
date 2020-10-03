b1ee1f431c844cc4498380db812827579305cf4b
isievwore <isievwore@systemspecs.com.ng>
2020-10-03 13:57:51 +0100

    Configured Environment Vaiables

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::PhotosController < ApplicationController
         page = params[:page]
         perPage = params[:perPage]
 
-        url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=736e2f4414385f87df595b0691038f22&tags=bikerace,BoulderBikeTour&page=#{page}&per_page=#{perPage}&format=json&nojsoncallback=1"
+        url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{ENV["FLICKR_API_KEY"]}&tags=bikerace,BoulderBikeTour&page=#{page}&per_page=#{perPage}&format=json&nojsoncallback=1"
         
         #Rest Client
         res = Faraday.get url
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Rider.create([ { firstname: "Dustin", lastname: "Green", city: "Hickory Hills", state: "IL", position: [40.00, -105.35] },
+{ firstname: "Jason", lastname: "Finn", city: "Huntington Beach", state: "CA", position: [39.95, -105.24] }, 
+{ firstname: "Maggie", lastname: "Lantz", city: "Dublin", state: "OH", position: [40.03, -105.23] },
+{ firstname: "Lawrence", lastname: "Duran", city: "Fort Myers", state: "FL", position: [40.04, -105.23] },
+{ firstname: "Irene", lastname: "Molina", city: "Tucson", state: "AZ", position: [39.96, -105.22] },
+{ firstname: "Nancy", lastname: "Garner", city: "Baltimore", state: "MD", position: [39.98, -105.21] },
+{ firstname: "Tara", lastname: "Taylor", city: "Minneapolis", state: "MN", position: [40.00, -105.25] },
+{ firstname: "Alejandro", lastname: "Smith", city: "Scarborough", state: "ME", position: [40.02, -105.26] },
+{ firstname: "Tricia", lastname: "Renshaw", city: "Mystic", state: "CT", position: [40.06, -105.30] },
+{ firstname: "Travis", lastname: "Cook", city: "Twin Lakes", state: "WI", position: [40.06, -105.20] },
+{ firstname: "Joan", lastname: "Brooks", city: "Norfolk", state: "VA", position: [39.98, -105.24] },
+{ firstname: "Joseph", lastname: "Rodgers", city: "Wayne", state: "NJ", position: [39.99, -105.25] },
+{ firstname: "Matthew", lastname: "Gregson", city: "Brashear", state: "MO", position: [40.01, -105.29] },
+{ firstname: "Katie", lastname: "Dunlap", city: "Marion", state: "VA", position: [39.97, -105.27] },
+{ firstname: "Leo", lastname: "Howard", city: "Dallas", state: "TX", position: [39.97, -105.33] },
+{ firstname: "Maria", lastname: "Baisden", city: "Raleigh", state: "NC", position: [40.00, -105.29] },
+{ firstname: "Sandy", lastname: "Townsend", city: "Newport", state: "PA", position: [40.05, -105.30] },
+{ firstname: "Melinda", lastname: "Stephenson", city: "Kahului", state: "HI", position: [39.99, -105.31] },
+{ firstname: "Jason", lastname: "Nichols", city: "Alexandria", state: "VA", position: [40.01, -105.32] }
+])
